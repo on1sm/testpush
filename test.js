8da9ecbb6aff5dcb0a5237081125b03e008ac79a
utop1a <47589496+on1sm@users.noreply.github.com>
2024-08-20 15:09:49 +0800

    Update test.js

--- test.js
+++ test.js
@@ -286,7 +286,7 @@
   var WebApp = {};
   var webAppInitData = '', webAppInitDataUnsafe = {};
   var themeParams = {}, colorScheme = 'light';
-  var webAppVersion = '7.2';
+  var webAppVersion = '7.7';
   var webAppPlatform = 'unknown';
 
   if (initParams.tgWebAppData && initParams.tgWebAppData.length) {
